ecd6bcb114f142ea857ad242231aead0a7255042
Daniel Pereira <danielpaulpereira@protonmail.com>
2020-09-08 00:52:26 -0400

    edit

--- 01_print_sums/print_sums.cpp
+++ 01_print_sums/print_sums.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-
 using namespace std;
-
 int main() {
 	int x, y;	// Stores the user inputs.
 	int sum;	// Stores the sum.
-
 	// Prompt user for input:
 		cout << "Enter a number: ";
 	// Get first number x:

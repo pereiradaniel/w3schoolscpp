4a333c55afed1727125177e20c83679b764fcc7d
Daniel Pereira <danielpaulpereira@protonmail.com>
2020-09-08 01:06:46 -0400

    Change file structures.

---
+++ data_types/characters/ascii_values/ascii_values.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+	// Declare ASCII values:
+	char a = 65, b = 66, c = 67;
+	// Display chars:
+	cout << a;
+	cout << b;
+	cout << c << endl;
+	// End program
+	return 0;
+};
